91be9ac854233ac4ac7d33e4834e66627ac9ab1a
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-10 15:42:00 -0400

    Added Imports Windows.UI.Popups to be used when clicking OK to desplay a dialog message

--- Hello_vb/MainPage.xaml.vb
+++ Hello_vb/MainPage.xaml.vb
@@ -1,9 +1,15 @@
 ﻿' The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
+Imports Windows.UI.Popups
+
 ''' <summary>
 ''' An empty page that can be used on its own or navigated to within a Frame.
 ''' </summary>
 Public NotInheritable Class MainPage
     Inherits Page
 
+    Private Sub onClick(sender As Object, e As RoutedEventArgs) Handles ok.Click
+        Dim msg = New MessageDialog("Hello " + userName.Text)
+        msg.ShowAsync()
+    End Sub
 End Class
